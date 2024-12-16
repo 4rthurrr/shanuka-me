d6640dac037f88e45f2121f983fbef4c6f6a35f9
shanuka <shanukaamantha35@gmail.com>
2024-12-17 05:17:43 +0530

    patch

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/shanuka-me',
   reactStrictMode: true,
-  assetPrefix: '/shanuka-me/',
 };
 
 export default nextConfig;
