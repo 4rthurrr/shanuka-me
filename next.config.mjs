c022811a4f2ee4703f9af8fd447ec3c499985196
shanuka <shanukaamantha35@gmail.com>
2024-12-17 03:48:55 +0530

    Add GitHub Pages deployment workflow
    Add GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/shanuka-me',
   reactStrictMode: true,
+  assetPrefix: '/shanuka-me/',
 };
 
 export default nextConfig;
